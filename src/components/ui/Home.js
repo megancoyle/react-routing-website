6051b9067d2bf1114be73ab331e3011ee55fdd9e
Megan Coyle <mcoyle@usgbc.org>
2017-03-03 14:38:57 -0500

    Initial commit

---
+++ src/components/ui/Home.js
@@ -0,0 +1,11 @@
+import MainMenu from './MainMenu'
+
+const Home = () =>
+    <div className="home">
+        <MainMenu className="home-page-menu"/>
+        <div id="homebox">
+            <h1>Website Template</h1>
+        </div>
+    </div>
+
+export default Home
