8e7177ddaeb065371faf8a0b9bf560cf9b771d70
Megan Coyle <mcoyle@usgbc.org>
2017-03-03 16:29:39 -0500

    Mapping through data arrays

--- src/components/ui/Home.js
+++ src/components/ui/Home.js
@@ -2,7 +2,8 @@ import MainMenu from './MainMenu'
 
 const Home = () =>
     <div className="home">
-        <MainMenu className="home-page-menu"/>
+        <MainMenu
+        className="home-page-menu"/>
         <div id="homebox">
             <h1>Website Template</h1>
         </div>
